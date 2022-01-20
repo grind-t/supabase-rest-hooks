78a949bc4c5dca188ee55e27efafb8cadd6e340a
grind-t <grindt.gd@gmail.com>
2022-01-20 16:17:37 +0300

    feat: object keys conversion

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,4 +1,4 @@
-import { camelCase, snakeCase } from './utils';
+import { camelCase, camelKeys, snakeCase, snakeKeys } from './utils';
 import { test, expect } from '@jest/globals';
 
 test('camelCase', () => {
@@ -13,4 +13,54 @@ test('snakeCase', () => {
   const snake = ['lower_camel_case'];
 
   expect(camel.every((v, i) => snakeCase(v) === snake[i])).toBe(true);
+});
+
+test('camelKeys', () => {
+  const snakeObj = {
+    snake_case: true,
+  };
+  const camelObj = {
+    snakeCase: true,
+  };
+  const snakeArr = [snakeObj, snakeObj];
+  const camelArr = [camelObj, camelObj];
+  const snakeNested = {
+    snake_obj: snakeObj,
+    snake_arr: snakeArr,
+    snake_val: true,
+  };
+  const camelNested = {
+    snakeObj: camelObj,
+    snakeArr: camelArr,
+    snakeVal: true,
+  };
+
+  expect(camelKeys(snakeObj)).toEqual(camelObj);
+  expect(camelKeys(snakeArr)).toEqual(camelArr);
+  expect(camelKeys(snakeNested)).toEqual(camelNested);
+});
+
+test('snakeKeys', () => {
+  const camelObj = {
+    camelCase: true,
+  };
+  const snakeObj = {
+    camel_case: true,
+  };
+  const camelArr = [camelObj, camelObj];
+  const snakeArr = [snakeObj, snakeObj];
+  const camelNested = {
+    camelObj,
+    camelArr,
+    camelVal: true,
+  };
+  const snakeNested = {
+    camel_obj: snakeObj,
+    camel_arr: snakeArr,
+    camel_val: true,
+  };
+
+  expect(snakeKeys(camelObj)).toEqual(snakeObj);
+  expect(snakeKeys(camelArr)).toEqual(snakeArr);
+  expect(snakeKeys(camelNested)).toEqual(snakeNested);
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 export type DeepPartial<T> = T extends object
   ? {
       [P in keyof T]?: DeepPartial<T[P]>;
@@ -14,4 +16,21 @@ export function camelCase(snakeCase: string): string {
 export function snakeCase(camelCase: string): string {
   const regex = /[A-Z]/g;
   return camelCase.replace(regex, (match) => '_' + match.toLowerCase());
+}
+
+export function mapKeys(value: any, callback: (key: string) => string): any {
+  if (Array.isArray(value)) return value.map((v) => mapKeys(v, callback));
+  if (typeof value !== 'object') return value;
+  return Object.entries(value).reduce((acc, [key, value]) => {
+    acc[callback(key)] = mapKeys(value, callback);
+    return acc;
+  }, {} as Record<string, any>);
+}
+
+export function camelKeys(value: any): any {
+  return mapKeys(value, camelCase);
+}
+
+export function snakeKeys(value: any): any {
+  return mapKeys(value, snakeCase);
 }
