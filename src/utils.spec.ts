379eabe7b722c1ff53eea2769d583718558a1f75
grind-t <grindt.gd@gmail.com>
2022-01-20 16:30:45 +0300

    refactor: util tests

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,66 +1,40 @@
 import { camelCase, camelKeys, snakeCase, snakeKeys } from './utils';
 import { test, expect } from '@jest/globals';
 
-test('camelCase', () => {
-  const snake = ['snake_case', 'SCREAMING_SNAKE_CASE'];
-  const camel = ['snakeCase', 'screamingSnakeCase'];
+test('case conversion', () => {
+  const snake = ['simple', 'really_long_and_complex'];
+  const camel = ['simple', 'reallyLongAndComplex'];
 
   expect(snake.every((v, i) => camelCase(v) === camel[i])).toBe(true);
-});
-
-test('snakeCase', () => {
-  const camel = ['lowerCamelCase'];
-  const snake = ['lower_camel_case'];
-
   expect(camel.every((v, i) => snakeCase(v) === snake[i])).toBe(true);
 });
 
-test('camelKeys', () => {
+test('keys conversion', () => {
   const snakeObj = {
-    snake_case: true,
+    simple: true,
+    really_long_and_complex: true,
   };
   const camelObj = {
-    snakeCase: true,
+    simple: true,
+    reallyLongAndComplex: true,
   };
   const snakeArr = [snakeObj, snakeObj];
   const camelArr = [camelObj, camelObj];
   const snakeNested = {
-    snake_obj: snakeObj,
-    snake_arr: snakeArr,
-    snake_val: true,
+    nested_obj: snakeObj,
+    nested_arr: snakeArr,
+    simple: true,
   };
   const camelNested = {
-    snakeObj: camelObj,
-    snakeArr: camelArr,
-    snakeVal: true,
+    nestedObj: camelObj,
+    nestedArr: camelArr,
+    simple: true,
   };
 
   expect(camelKeys(snakeObj)).toEqual(camelObj);
-  expect(camelKeys(snakeArr)).toEqual(camelArr);
-  expect(camelKeys(snakeNested)).toEqual(camelNested);
-});
-
-test('snakeKeys', () => {
-  const camelObj = {
-    camelCase: true,
-  };
-  const snakeObj = {
-    camel_case: true,
-  };
-  const camelArr = [camelObj, camelObj];
-  const snakeArr = [snakeObj, snakeObj];
-  const camelNested = {
-    camelObj,
-    camelArr,
-    camelVal: true,
-  };
-  const snakeNested = {
-    camel_obj: snakeObj,
-    camel_arr: snakeArr,
-    camel_val: true,
-  };
-
   expect(snakeKeys(camelObj)).toEqual(snakeObj);
+  expect(camelKeys(snakeArr)).toEqual(camelArr);
   expect(snakeKeys(camelArr)).toEqual(snakeArr);
+  expect(camelKeys(snakeNested)).toEqual(camelNested);
   expect(snakeKeys(camelNested)).toEqual(snakeNested);
 });
